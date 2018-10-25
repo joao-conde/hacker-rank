a5892847c7b22252f3c6aba8cee9341572850c5b
João Conde <up201503256@fe.up.pt>
2018-10-25 21:07:55 +0100

    some more super basic c++ tutorials, just farming Hackos :D

---
+++ language-proficiency/c++/strings.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    cout << a.size() << " " << b.size() << endl;
+    cout << a << b << endl;
+
+    char fa = a[0];
+    a[0] = b[0];
+    b[0] = fa;
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
