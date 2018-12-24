a6bb01443ddad45ce77e03ba343d80092db145af
João Conde <up201503256@fe.up.pt>
2018-12-24 17:58:37 +0000

    easy hackos done

---
+++ language-proficiency/c/easy/bitwise-operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int n, k;
+	scanf("%d %d", &n, &k);
+	
+	int andMax = 0, orMax = 0, xorMax = 0;
+	for(int i = 1; i <= n; i++){
+		for(int j = i+1; j <= n; j++){
+			int and = i & j, or = i | j, xor = i ^ j;
+			
+			andMax = (and < k && and > andMax ? and : andMax);
+			orMax = (or < k && or > orMax ? or : orMax);
+			xorMax = (xor < k && xor > xorMax ? xor : xorMax);
+		}	
+	}
+
+	printf("%d\n%d\n%d", andMax, orMax, xorMax);
+}
