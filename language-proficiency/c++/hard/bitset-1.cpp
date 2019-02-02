4b643e4ddea55bd8eab7d3d77a5620819ba92233
João Conde <up201503256@fe.up.pt>
2019-02-02 14:27:15 +0000

    working for small values, overflow with limits

--- language-proficiency/c++/hard/bitset-1.cpp
+++ language-proficiency/c++/hard/bitset-1.cpp
@@ -3,31 +3,26 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-ull getNext(ull prev, ull p, ull q, ull mod){
-	return prev * p + q % mod;
-}
+ull getNext(ull prev, ull p, ull q, ull mod) { return prev * p + q % mod; }
 
 int main() {
-	int n, s, p, q;
-	cin >> n >> s >> p >> q;
-	
-	// a[0] = S (modulo 2^31)
-	// for i = 1 to N-1
-	// 	a[i] = a[i-1]*P+Q (modulo 2^31) 
+    int n, s, p, q;
+    cin >> n >> s >> p >> q;
 
-	//floyd's tortoise and hare algorithm
-	ull tortoise, hare, mod = pow(2, 31);	
-	tortoise = getNext(s % mod, p, q, mod); 
-	hare = getNext(tortoise, p, q, mod);
-	 
-	for(int i = 1; i <= n-1; i++){
+    // a[0] = S (modulo 2^31)
+    // for i = 1 to N-1
+    // 	a[i] = a[i-1]*P+Q (modulo 2^31)
+
+    // floyd's tortoise and hare algorithm
+    ull tortoise, hare, mod = pow(2, 31);
+    tortoise = getNext(s % mod, p, q, mod); //second val
+    hare = getNext(tortoise, p, q, mod);   	//third val
+
+	while(tortoise != hare){
 		tortoise = getNext(tortoise, p, q, mod);
-		hare = getNext(getNext(hare, p, q, mod), p, q, mod);  
-		if(tortoise == hare){
-			cout << hare << endl;
-			return 0;
-		}
+		hare = getNext(getNext(hare, p, q, mod), p, q, mod);
 	}
 
-	cout << n << endl;
+	cout << "tort: " << tortoise
+			<< "hare: " << hare << endl;
 }
