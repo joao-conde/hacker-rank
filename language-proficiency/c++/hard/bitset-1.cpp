16e34ade8078ab164641f0a33671176ea04757c9
João Conde <up201503256@fe.up.pt>
2019-02-02 15:36:09 +0000

    failling 2 test cases only

--- language-proficiency/c++/hard/bitset-1.cpp
+++ language-proficiency/c++/hard/bitset-1.cpp
@@ -3,26 +3,46 @@ using namespace std;
 
 typedef unsigned long long ull;
 
+
 ull getNext(ull prev, ull p, ull q, ull mod) { return prev * p + q % mod; }
 
 int main() {
     int n, s, p, q;
     cin >> n >> s >> p >> q;
 
-    // a[0] = S (modulo 2^31)
-    // for i = 1 to N-1
-    // 	a[i] = a[i-1]*P+Q (modulo 2^31)
-
     // floyd's tortoise and hare algorithm
-    ull tortoise, hare, mod = pow(2, 31);
-    tortoise = getNext(s % mod, p, q, mod); //second val
+    ull tortoise, hare, mod = pow(2, 31), initial = s % mod;
+    tortoise = getNext(initial, p, q, mod); //second val
     hare = getNext(tortoise, p, q, mod);   	//third val
 
+	//cycle detection
+	int iter = 0;
 	while(tortoise != hare){
+		if(iter > n){
+			cout << n << endl;
+			return 0;
+		}
 		tortoise = getNext(tortoise, p, q, mod);
 		hare = getNext(getNext(hare, p, q, mod), p, q, mod);
+		iter++;
 	}
 
-	cout << "tort: " << tortoise
-			<< "hare: " << hare << endl;
+	//determine cyle start
+	ull preCycleLen = 0;
+	tortoise = initial;
+	while(tortoise != hare){
+		tortoise = getNext(tortoise, p, q, mod);
+		hare = getNext(hare, p, q, mod);
+		preCycleLen++;
+	}
+	
+	//shortest cycle length
+	ull cycleLen = 1;
+	hare = getNext(tortoise, p, q, mod);
+	while(tortoise != hare){
+		hare = getNext(hare, p, q, mod);
+		cycleLen++;
+	}
+
+	cout << preCycleLen + cycleLen << endl;
 }
